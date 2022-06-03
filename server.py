e5286c9db3fbfd3a47c568112b62b712537283bd
riazbd <nuruddinriaz5@gmain.com>
2022-06-03 22:37:41 +0000

    created passport api

--- server.py
+++ server.py
@@ -285,5 +285,176 @@ def api_id_card_info():
         return jsonify(response)
 
 
+
+# for passport
+
+@app.route('/passport-info-extractor', methods=['POST'])
+def api_passport_info():
+    app.logger.info(PROJECT_HOME)
+    if (request.method == 'POST' and request.files['front-image']):
+        app.logger.info(app.config['UPLOAD_FOLDER'])
+        img = request.files['front-image']
+        img_name = secure_filename(img.filename)
+        create_new_folder(app.config['UPLOAD_FOLDER'])
+        saved_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+        # app.logger.info("saving {}".format(saved_path))
+        img.save(saved_path)
+        mrz = read_mrz(saved_path)
+        print(mrz)
+
+        # Person detection from Identity Card
+
+        detector = ObjectDetection()
+        detector.setModelTypeAsYOLOv3()
+        detector.setModelPath(f'{PROJECT_HOME}/models/yolo.h5')
+        detector.loadModel()
+        detections = detector.detectObjectsFromImage(input_image=saved_path,
+                                                     output_image_path=os.path.join(PROJECT_HOME, "newDetected", "imagenew.jpg"),
+                                                     minimum_percentage_probability=60)
+                                                     
+        print(detections)
+
+        if detections != []:
+          if detections[0]["name"] == 'person':
+            isPerson = True
+            personPercentageProbability = f"{detections[0]['name']} {detections[0]['percentage_probability']}"
+            personBoxPoints = detections[0]["box_points"]
+            
+        # Read front side
+            
+        path_to_tesseract = r"/usr/bin/tesseract"
+        
+        img_open = Image.open(saved_path)
+        pytesseract.tesseract_cmd = path_to_tesseract
+        
+        front_text = pytesseract.image_to_string(img_open)
+        
+        final_front_text = front_text[:-1]
+            
+        #End Read front side
+
+        # END Person detection from Identity Card
+        try:
+            if mrz is not None:
+                mrz_data = mrz.to_dict()
+                firstName = mrz_data['names']
+                deleted_substring = firstName.split("  ", 1)[0]
+                firstName = deleted_substring
+                lastName = mrz_data['surname']
+                dateOfBirth = mrz_data['date_of_birth']
+                dateOfBirth = datetime.strptime(dateOfBirth, '%y%m%d').strftime('%d/%m/%Y')
+                exp_date = mrz_data['expiration_date']
+                exp_date = datetime.strptime(exp_date, '%y%m%d').strftime('%d/%m/%Y')
+                country = mrz_data['country']
+                nationality = mrz_data['nationality']
+                gender = mrz_data['sex']
+                idNo = mrz_data['number']
+                
+                # Read front side
+                
+                # path_to_tesseract = r"/usr/bin/tesseract"
+                
+                # img_open = Image.open(saved_path_front)
+                # pytesseract.tesseract_cmd = path_to_tesseract
+                
+                # front_text = pytesseract.image_to_string(img_open)
+                
+                # final_front_text = front_text[:-1]
+                
+                # #End Read front side
+    
+    
+                if (firstName.lower() in final_front_text.lower()) or (lastName.lower() in final_front_text.lower()) or (idNo.lower() in final_front_text.lower()):
+    
+                    response = {
+                        "data": [
+                            {
+                                "parsed_data": [
+                                
+                                  mrz_data,
+                                
+                            ],
+                                "message": "Successfully Read",
+                                "front_text": final_front_text,
+                                "first_name": firstName,
+                                "last_name": lastName,
+                                "date_of_birth": dateOfBirth,
+                                "exp_date": exp_date,
+                                "gender": gender,
+                                "country": country,
+                                "nationality": nationality,
+                                "id_no": idNo,
+                                "isPerson": isPerson,
+                                "personPercentageProbability": personPercentageProbability,
+                                "personBoxPoints": personBoxPoints
+        
+                            }
+                        ]
+                    }
+        
+                    return jsonify(response)
+                    
+                else:
+                
+                    response = {
+                    
+                      "data": [
+                            {
+                            "message": "Invalid Id Card."
+                            }
+                          ]
+                        }
+                
+                    return jsonify(response)
+                    
+    
+            elif mrz is None:
+            #else:
+                response = {
+                    "data": [
+                        {
+                            "message": "Sorry we couldn't read your provided Id card. Please ensure the text of your document is clear to read."
+                        }
+                    ]
+                }
+                
+                return jsonify(response)
+            else:
+                response = {
+                    "data": [
+                        {
+                            "message": "Sorry, we encountered an unknown problem."
+                        }
+                    ]
+                }
+                
+                return jsonify(response)
+        except:
+        
+              response = {
+                    "data": [
+                        {
+                            "message": "Sorry, something is wrong."
+                        }
+                    ]
+                }
+                
+              return jsonify(response)
+            
+            
+        
+    # return send_from_directory(app.config['UPLOAD_FOLDER'],img_name, as_attachment=True)
+    else:
+        response = {
+                    "data": [
+                        {
+                            "message": "Please provide all documents."
+                        }
+                    ]
+                }
+                
+        return jsonify(response)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
